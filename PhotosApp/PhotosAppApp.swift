78535be2aa686d86c57ba21b3dd3afaabc8f7833
brainox <macmartins081@gmail.com>
2021-11-14 14:08:13 +0100

    Initial Commit

---
+++ PhotosApp/PhotosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosAppApp.swift
+//  PhotosApp
+//
+//  Created by Decagon on 14/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct PhotosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
